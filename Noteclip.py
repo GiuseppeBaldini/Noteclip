8ca81d5137173ff6a33b41de4346f23167440ff1
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2018-09-24 12:45:35 +0200

    Fix working directory path

--- Noteclip.py
+++ Noteclip.py
@@ -1,15 +1,16 @@
-# Notecut: a simple program to get notes pasted directly in your clipboard
+# Python 3
+# Noteclip: a simple program to get notes pasted directly in your clipboard
 
 import os, sys, pyperclip
 
 note_name = sys.argv[1]
 file = note_name.lower() + ".txt"
-current_dir = os.getcwd()
+working_dir = os.chdir('C:\\Users\\Giuseppe\\Documents\\Notes')
 
-dir_list = os.listdir(current_dir)
+dir_list = os.listdir(working_dir)
 
 if file in dir_list:
-    file_text = open(file, 'r')
+    file_text = open(file)
     content = file_text.read()
     pyperclip.copy(content)
     print('The content of ' + file + ' has been copied to the clipboard.')
