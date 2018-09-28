0dc922994feb4a7780419a3b7adb2a953551dfc8
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2018-09-28 12:11:04 +0200

    Variable name changes

--- Noteclip.py
+++ Noteclip.py
@@ -14,16 +14,15 @@ try:
 except IndexError:
     note_name = input('Please input the name of your note here: > ')
 
-file = note_name.lower() + ".txt"
+file_name = note_name.lower() + ".txt"
 working_dir = os.chdir('C:\\Users\\Giuseppe\\Documents\\Notes')
-
 dir_list = os.listdir(working_dir)
 
-if file in dir_list:
-    file_text = open(file)
+if file_name in dir_list:
+    file_text = open(file_name)
     content = file_text.read()
     pyperclip.copy(content)
-    print('The content of ' + file + ' has been copied to the clipboard.')
+    print('The content of ' + file_name + ' has been copied to the clipboard.')
     file_text.close()
 else:
-    print('There is no note called ' + file + ' in this folder.')
+    print('There is no note called ' + file_name + ' in this folder.')
