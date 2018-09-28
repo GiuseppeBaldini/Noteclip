1126ca5131ee2835e11b9ff3ed21fed271043c67
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2018-09-28 17:24:10 +0200

    Initial commit

--- Noteclip.py
+++ Noteclip.py
@@ -1,18 +1,43 @@
-# Notecut: a simple program to get notes pasted directly in your clipboard
+# Python 3
+# Noteclip: a simple program to get notes pasted directly in your clipboard
 
-import os, sys, pyperclip
+import sys
+import os
 
-note_name = sys.argv[1]
-file = note_name.lower() + ".txt"
-current_dir = os.getcwd()
+try:
+    import pyperclip
+except ImportError or ModuleNotFoundError:
+    print('Pyperclip module not found. Please download it.')
 
-dir_list = os.listdir(current_dir)
+try:
+    note_name = sys.argv[1]
+except IndexError:
+    note_name = input('Please input the name of your note here: > ')
 
-if file in dir_list:
-    file_text = open(file, 'r')
+starting_dir = os.getcwd()
+file_name = note_name + ".txt"
+notes_dir = 'C:\\Users\\Giuseppe\\Documents'
+
+def find(file, path):
+    for root, dirs, files in os.walk(notes_dir):
+        if file_name in files:
+            print (file_name +' found in ' + root)
+            new_working_dir = root
+            return new_working_dir
+        else:
+            pass
+
+nwd = find(file_name, notes_dir)
+working_dir = os.chdir(nwd)
+dir_list = os.listdir(nwd)
+
+if file_name in dir_list:
+    file_text = open(file_name)
     content = file_text.read()
     pyperclip.copy(content)
-    print('The content of ' + file + ' has been copied to the clipboard.')
+    print('The content of ' + file_name + ' has been copied to the clipboard.')
     file_text.close()
 else:
-    print('There is no note called ' + file + ' in this folder.')
+    print('There is no file called ' + file_name + ' in this folder.')
+
+os.chdir(starting_dir)
