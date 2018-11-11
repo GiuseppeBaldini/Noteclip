682118e98b016d859849e01a65dfbd0664076c84
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2018-11-11 09:55:24 +0100

    Prompt user until file is found

--- Noteclip.py
+++ Noteclip.py
@@ -1,28 +1,65 @@
-# Python 3
-# Noteclip: a simple program to get notes pasted directly in your clipboard
+#! python3
+
+# Noteclip: a simple program to retrieve notes directly in the clipboard
 
 import sys
 import os
 
+# Pyperclip is not a built-in module, remind users to download it if necessary
 try:
     import pyperclip
-except ImportError or ModuleNotFoundError:
+except (ImportError, ModuleNotFoundError):
     print('Pyperclip module not found. Please download it.')
+    exit()
 
+# If the argument is missing, let users input it directly
 try:
     note_name = sys.argv[1]
 except IndexError:
     note_name = input('Please input the name of your note here: > ')
 
-file_name = note_name.lower() + ".txt"
-working_dir = os.chdir('C:\\Users\\Giuseppe\\Documents\\Notes')
-dir_list = os.listdir(working_dir)
+# Define 3 key variables: current working directory, file name and path to scan
+start_working_dir = os.getcwd()
+file_name = note_name + ".txt"
+notes_path = r'C:\Users\Giuseppe\Documents'
 
-if file_name in dir_list:
-    file_text = open(file_name)
+def find(file, path):
+    """
+    Return the folder where a given file is located by scanning every subfolder
+    of the given path. In this case, assuming files are more likely to be
+    at the end of the path, it is faster to scan bottom-up.
+    """
+    for root, dirs, files in os.walk(path, topdown = False):
+        if file in files:
+            print (file +' found in ' + root)
+            temp_working_dir = root
+            return temp_working_dir
+    else:
+        print('There is no file called ' + file_name + ' in ' + path +
+            '\nPlease make sure spelling and capitalisation are correct.')
+        return None
+
+# Prompt for a note until the file is found
+while True:
+    twd = find(file_name, notes_path)
+    if twd == None:
+        file_name = str(input('Please enter note name: > ') + '.txt')
+        continue
+    else:
+        break
+
+# Temporarily change the working directory to the one where we found the file
+os.chdir(twd)
+
+def copy(the_file):
+    """ Open file, copy content to the clipboard using pyperclip, close it."""
+    file_text = open(the_file)
     content = file_text.read()
     pyperclip.copy(content)
-    print('The content of ' + file_name + ' has been copied to the clipboard.')
+    print('The content of ' + the_file + ' has been copied to the clipboard.')
     file_text.close()
-else:
-    print('There is no note called ' + file_name + ' in this folder.')
+
+copy(file_name)
+
+# Come back to the starting working directory
+os.chdir(start_working_dir)
